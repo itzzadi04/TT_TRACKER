f89e4847db9408d8f8f3804b6782fbd0e4664a51
Aditya Sharma <itzzadi04@gmail.com>
2026-08-21 10:09:31 +0530

    sm typo

--- index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const RoomSchema = new mongoose.Schema({
-  roomNo: { type: String, required: true, unique: true },
-  labOrClass: { type: String, enum: ['Lab', 'Class'], required: true },
-  building: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Room', RoomSchema);
